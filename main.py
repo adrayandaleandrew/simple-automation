7095f29a02f504e9c6d9e224be6f85f8e9be4123
Dale Adrayan <adrayandaleandrew@gmail.com>
2024-07-21 00:46:20 +0300

    some minor changes

--- main.py
+++ main.py
@@ -3,31 +3,31 @@ import random
 import threading
 
 
-
-
 def autoMove():
-    threading.Timer(20, autoMove).start()
+    threading.Timer(300, autoMove).start()
     pyautogui.click(100, 100)
-# pyautogui.moveTo(100, 100, duration = 2)
-    while(True):
-        random_scroll = random.randint(-1000, +1000)
-        x_axis = random.randint(100,1920)
-        y_axis = random.randint(100,1080)
+
+  
+    random_scroll = random.randint(-1000, +1000)
+    x_axis = random.randint(100,1920)
+    y_axis = random.randint(100,1080)
 
 
-        pyautogui.moveTo(x_axis, y_axis, duration = 2)
-        pyautogui.moveRel(0, 50, duration = 2)
-        pyautogui.scroll(random_scroll)
+    pyautogui.moveTo(x_axis, y_axis, duration = 2)
+    pyautogui.moveRel(0, 50, duration = 2)
+    pyautogui.scroll(random_scroll)
 
-        print(pyautogui.position())
+    print(pyautogui.position())
 
 
-        pyautogui.hotkey('ctrl', 'shift', 'tab')
+    pyautogui.hotkey('ctrl', 'shift', 'tab')
 
 
 
 autoMove()
 
+
+#   pyautogui.moveTo(100, 100, duration = 2)
 
 # while(True):
 #         random_scroll = random.randint(-1000, +1000)
